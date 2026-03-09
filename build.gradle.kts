cb78110516f71da25ef0d6c5f721be087096bae0
imengying <yuzucv@gmail.com>
2026-03-09 18:11:06 +0800

    调整

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,20 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin") {
+            version {
+                strictly("2.3.10")
+            }
+        }
+        classpath("com.google.devtools.ksp:symbol-processing-gradle-plugin:2.3.4")
+    }
+}
+
 plugins {
     id("com.android.application") version "9.1.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.3.10" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.3.10" apply false
     id("com.google.devtools.ksp") version "2.3.4" apply false
 }
